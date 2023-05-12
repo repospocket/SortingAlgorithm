85de8b5a5e93ead1d42ad984ebef6e066b393abe
George <126033073+repospocket@users.noreply.github.com>
2023-05-12 14:05:07 -0400

    Reformat code / styles.css

--- Sorting_Visualization/Sorting_app/TrieNodeSort.py
+++ Sorting_Visualization/Sorting_app/TrieNodeSort.py
@@ -4,18 +4,16 @@ class Node:
     def __init__(self, value=None):
         self.val = value
         self.leaves = []
-        self.repeated_numbers_number = 0  # for repeated numbers in the list to be sorted
+        self.repeated_numbers_count = 0  # for repeated numbers in the list to be sorted
         self.Leaves_length = 0  # length of current self.leaves to not use len()
-        #self.x_loc = 0
-        #self.y_loc = 0
 
 class NodeEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Node):
             # Convert Node object to a dictionary
-            return {'digit': obj.val , 'children': obj.leaves}
+            return {'digit': obj.val, 'children': obj.leaves}
         return super().default(obj)
-    #, 'loc': [ obj.x_loc, obj.y_loc ]
+
 class TrieNodeSort:
     def int_to_list(self, num):  # 469 -> [9,6,4]
         numlist = []
@@ -38,7 +36,7 @@ class TrieNodeSort:
             numlist.append(0)
             pad -= 1
 
-    def insert_into_trie(self, numlist, parent, json_data):
+    def insert_into_trie(self, numlist, parent):
         if numlist:
             digit = numlist.pop()
             lenL = max(0, parent.Leaves_length - 1)
@@ -48,91 +46,73 @@ class TrieNodeSort:
                     leaf = parent.leaves[i]
 
                     if leaf.val == digit:
-                        
-                        self.insert_into_trie(numlist, leaf,json_data)
+                        self.insert_into_trie(numlist, leaf)
                         break
 
                     elif leaf.val > digit:
                         if i == 0:
                             node = Node(digit)
-                            #node.x_loc = leaf.x_loc - 1
-                            #node.y_loc = leaf.y_loc 
-
-                            
-                            print(digit)
                             parent.leaves.insert(0, node)
-                            json_data.append(node)
-                            
                             parent.Leaves_length += 1
-                            self.insert_into_trie(numlist, node,json_data)
+                            self.insert_into_trie(numlist, node)
                             break
                         i -= 1
 
                     elif leaf.val < digit:
                         node = Node(digit)
-                        #node.x_loc = leaf.x_loc + 1
-                        #node.y_loc = leaf.y_loc 
-
                         parent.leaves.insert(i + 1, node)
-                        json_data.append(node)
-
-
                         parent.Leaves_length += 1
-                        self.insert_into_trie(numlist, node,json_data)
+                        self.insert_into_trie(numlist, node)
                         break
             else:
                 node = Node(digit)
                 parent.leaves.append(node)
                 parent.Leaves_length += 1
-                self.insert_into_trie(numlist, node,json_data)
+                self.insert_into_trie(numlist, node)
                 print(digit)
         else:
-            parent.repeated_numbers_number += 1
+            parent.repeated_numbers_count += 1
 
     def print_trie(self, node, list, sum=0):
         sum += node.val
-
         if node.leaves:
             for num in node.leaves:
                 self.print_trie(num, list, sum * 10)
         else:
-            while node.repeated_numbers_number > 0:
+            while node.repeated_numbers_count > 0:
                 list.append(sum)
-                node.repeated_numbers_number -= 1
+                node.repeated_numbers_count -= 1
 
         return list
-    
-# Custom JSON encoder
+
     def sort_list(self, nums):
-        json_data = []
         self.maxlen = 0
         result = Node(0)
         list = []
         final = []
-        nodes_dict = {}
-       # nodes_dict[str(node.val)] = (100, 100)
         for num in nums:
             list.append(self.int_to_list(num))
         for numlist in list:
             self.pad_w_zeros(numlist)
-            self.insert_into_trie(numlist, result, json_data)
+            self.insert_into_trie(numlist, result)
         self.print_trie(result, final)
-        return [result,final]
+        return [result, final]
 
 def main():
     while True:
-            try:
-                user_input = input("Enter a list of integers separated by commas: ")
-                integer_list = [int(x.strip()) for x in user_input.split(",")]
-                sorted_list = TrieNodeSort().sort_list(integer_list)
-                print("The sorted list is:", sorted_list)
-                return
-            
-            except ValueError:
-                print("Error: Invalid input. Please enter a list of integers separated by commas.")
-            
-            except Exception as e:
-                print("An error occurred:", e)
+        try:
+            user_input = input("Enter a list of integers separated by commas: ")
+            integer_list = [int(x.strip()) for x in user_input.split(",")]
+            sorted_list = TrieNodeSort().sort_list(integer_list)
+            print("The sorted list is:", sorted_list)
+            return
+
+        except ValueError:
+            print(
+                "Error: Invalid input. Please enter a list of integers separated by commas.")
+
+        except Exception as e:
+            print("An error occurred:", e)
 
 if __name__ == "__main__":
     main()
--- Sorting_Visualization/Sorting_app/static/js/matter_visualization.js
+++ Sorting_Visualization/Sorting_app/static/js/matter_visualization.js
@@ -1,130 +1,128 @@
+var renderContainer = document.getElementById("render-container");
 var engine = Matter.Engine.create();
 var render = Matter.Render.create({
-  element: document.body,
+  element: renderContainer,
   engine: engine,
   options: {
     width: 800,
     height: 600,
     wireframes: false,
-    background: 'skyblue',  
+    background: 'skyblue',
   }
 });
-
-var radius = 30;
-var rootX = 400;
-var rootY = 210;
 
 var mouse = Matter.Mouse.create(render.canvas);
 var mouseConstraint = Matter.MouseConstraint.create(engine, {
   mouse: mouse,
-  constraint: {
-    stiffness: 1
-  }
-
 });
-
-var rootCircle = null; // Declare rootCircle variable
 
 Matter.Runner.run(engine);
 Matter.Render.run(render);
 
 function renderTextLabels(render) {
-    var ctx = render.context;
-    ctx.font = '20px Arial';
-    ctx.fillStyle = 'black';
-  
-    Matter.Composite.allBodies(render.engine.world).forEach(function (body) {
-      if (body.label) {
-        var position = body.position;
-        var label = body.label ;
-  
-        // Update the text position relative to the body's position
-        var textX = position.x - 3;
-        var textY = position.y + 3;
-  
-        // Center the text horizontally
-        var textWidth = ctx.measureText(label).width;
-        textX -= textWidth / 2;
-  
-        // Render the text label at the updated position
-        ctx.fillText(label, textX, textY);
+  var ctx = render.context;
+  ctx.font = '20px Arial';
+  ctx.fillStyle = 'black';
+
+  Matter.Composite.allBodies(render.engine.world).forEach(function (body) {
+
+    if (body.label) {
+
+      var position = body.position;
+      var label = body.label;
+
+      // Update the text position relative to the body's position
+      var textX = position.x - 3;
+      var textY = position.y + 3;
+
+      // Center the text horizontally
+      var textWidth = ctx.measureText(label).width;
+      textX -= textWidth / 2;
+
+      // Render the text label at the updated position
+      ctx.fillText(label, textX, textY);
+
+    }
+  });
+}
+
+function setupMouseEvents(circle) {
+
+  render.canvas.addEventListener("mousedown", function (event) {
+    if (event.target === render.canvas) {
+      var mousePosition = mouse.position;
+      if (Matter.Bounds.contains(circle.bounds, mousePosition)) {
+        mouseConstraint.body = circle;
+        Matter.Body.setStatic(circle, true);
       }
-    });
-  }
+    }
+  });
+
+  render.canvas.addEventListener("mousemove", function (event) {
+    if (mouseConstraint.body === circle) {
+      var mousePosition = mouse.position;
+      Matter.Body.setPosition(circle, mousePosition);
+    }
+  });
+
+  render.canvas.addEventListener("mouseup", function (event) {
+    mouseConstraint.body = null;
+    Matter.Body.setStatic(circle, false);
+  });
+}
 
 function renderNodes(data) {
 
-    function createCircle(x, y, isStatic) {
-      var circle = Matter.Bodies.circle(x, y, radius, { isStatic: isStatic });
+  function createCircle(x, y, isStatic) {
 
-  
-      if (!isStatic) {
-        
-        Matter.Events.on(render, 'afterRender', function () {
-          renderTextLabels(render);
-        });
+    var circle = Matter.Bodies.circle(x, y, radius, { isStatic: isStatic });
 
-        render.canvas.addEventListener("mousedown", function(event) {
-          if (event.target === render.canvas) {
-            var mousePosition = mouse.position;
-            if (Matter.Bounds.contains(circle.bounds, mousePosition)) {
-              mouseConstraint.body = circle;
-              Matter.Body.setStatic(circle, true);
-            }
-          }
-        });
-        
-        render.canvas.addEventListener("mousemove", function(event) {
-          if (mouseConstraint.body === circle) {
-            var mousePosition = mouse.position;
-            Matter.Body.setPosition(circle, mousePosition);
-          }
-        });
-        
-        render.canvas.addEventListener("mouseup", function(event) {
-          mouseConstraint.body = null;
-          Matter.Body.setStatic(circle, false);
-        });
-        Matter.World.add(engine.world, [circle, mouseConstraint]);
-        
-
-      }else{
-        Matter.World.add(engine.world, [circle]);
-      }
-      return circle;
-    }
-  
-    rootCircle = createCircle(rootX, rootY, true); // Assign value to rootCircle
-    rootCircle.label = '0';
-  
-    function createChildCircles( parent, children) {
-      var angleStep = Math.PI * 2 / children.length;
-      var distance = 100;
-  
-      children.forEach(function (child, index) {
-        var angle = index * angleStep;
-        var childX = parent.position.x + Math.cos(angle) * distance;
-        var childY = parent.position.y + Math.sin(angle) * distance;
-        var childCircle = createCircle(childX, childY, false);
-        childCircle.label = child.digit || "0";
-  
-        Matter.World.add(engine.world, [
-          Matter.Constraint.create({
-            bodyA: parent,
-            pointA: { x: 0, y: 0 },
-            bodyB: childCircle,
-            pointB: { x: 0, y: 0 },
-            stiffness: 1,
-            length: distance
-          })
-        ]);
-  
-        createChildCircles( childCircle, child.children);
+    if (!isStatic) {
+      Matter.Events.on(render, 'afterRender', function () {
+        renderTextLabels(render);
       });
+      setupMouseEvents(circle);
+      Matter.World.add(engine.world, [circle, mouseConstraint]);
+    } else {
+      Matter.World.add(engine.world, [circle]);
     }
-  
-    createChildCircles( rootCircle, data.children);
-  
-    
+
+    return circle;
   }
-  
+
+  var radius = 30;
+  var rootX = 400;
+  var rootY = 100;
+  var rootCircle = null; // Declare rootCircle variable
+  rootCircle = createCircle(rootX, rootY, true); // Assign value to rootCircle
+  rootCircle.label = '0';
+
+  function createChildCircles(parent, children) {
+    var angleStep = Math.PI * 2 / children.length;
+    var distance = 100;
+
+    children.forEach(function (child, index) {
+      var angle = index * angleStep;
+      var childX = parent.position.x + Math.cos(angle) * distance;
+      var childY = parent.position.y + Math.sin(angle) * distance;
+      var childCircle = createCircle(childX, childY, false);
+      childCircle.label = child.digit || "0";
+
+      Matter.World.add(engine.world, [
+        Matter.Constraint.create({
+          bodyA: parent,
+          pointA: { x: 0, y: 0 },
+          bodyB: childCircle,
+          pointB: { x: 0, y: 0 },
+          stiffness: 1,
+          length: distance
+        })
+      ]);
+
+      createChildCircles(childCircle, child.children);
+    });
+  }
+
+  createChildCircles(rootCircle, data.children);
+
+}
